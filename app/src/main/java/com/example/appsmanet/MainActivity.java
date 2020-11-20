ab2541f7f5eb362b5a691855567a989bd1b4731a
marcosravagnani <72414608+marcosravagnani@users.noreply.github.com>
2020-11-19 23:49:05 -0300

    Start commit!

--- app/src/main/java/com/example/appsmanet/MainActivity.java
+++ app/src/main/java/com/example/appsmanet/MainActivity.java
@@ -2,25 +2,33 @@ package com.example.appsmanet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btentrar;
-    EditText login, senha;
+    Button btn_entrar;
+    EditText edt_login, edt_senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_login);
-        btentrar = (Button) findViewById(btentrar);
-        login = (EditText) findViewById(login);
-        senha = (EditText) findViewById(senha);
 
-        btentrar.setOnClickListener((View) -> {
+        edt_login = (EditText) findViewById(R.id.edt_login);
+        edt_senha = (EditText) findViewById(R.id.edt_senha);
+        btn_entrar = (Button) findViewById(R.id.btn_entrar);
 
-        });
+    }
+
+    public void valida (View view){
+        if (edt_login.equals("admin")&& edt_senha.equals("admin")){
+            startActivity(new Intent(MainActivity.this, Tela_Principal.class));
+        }
+        else
+            Toast.makeText(this, "usuário ou senha inválido", Toast.LENGTH_LONG).show();
     }
 }
